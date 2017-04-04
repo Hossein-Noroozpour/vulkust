afb228bcc8da4cbef5a8dcaa050e094378d663c7
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-04-04 16:25:02 +0430

    going to sleep

--- vulkust/src/vulkan/command/pool.rs
+++ vulkust/src/vulkan/command/pool.rs
@@ -1,16 +1,5 @@
-use super::super::super::system::vulkan::{
-    VkResult,
-    VkCommandPool,
-    VkStructureType,
-    vkCreateCommandPool,
-    vkDestroyCommandPool,
-    VkAllocationCallbacks,
-    VkCommandPoolCreateInfo,
-    VkCommandPoolCreateFlagBits,
-};
-
-use super::super::device::Device;
-
+use super::super::super::system::vulkan as vk;
+use super::super::device::logical::Logical as LogicalDevice;
 use std::default::Default;
 use std::sync::{
     Arc,
@@ -18,16 +7,17 @@ use std::sync::{
 };
 
 pub struct Pool {
-    pub device: Arc<Device>,
-    pub vk_pool: VkCommandPool,
+    pub logical_device: Arc<LogicalDevice>,
+    pub vk_data: vk::VkCommandPool,
 }
 
 impl Pool {
-    pub fn new(device: Arc<Device>, queue_family_index: u32) -> Self {
-        let vk_cmd_pool_info = VkCommandPoolCreateInfo::default();
-        vk_cmd_pool_info.sType = VkStructureType::VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    pub fn new(logical_device: Arc<LogicalDevice>, queue_family_index: u32) -> Self {
+        let vk_cmd_pool_info = vk::VkCommandPoolCreateInfo::default();
+        vk_cmd_pool_info.sType = vk::VkStructureType::VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
         vk_cmd_pool_info.queueFamilyIndex = queue_family_index;
-        vk_cmd_pool_info.flags = VkCommandPoolCreateFlagBits::VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT as u32;
+        vk_cmd_pool_info.flags =
+            vk::VkCommandPoolCreateFlagBits::VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT as u32;
         let mut vk_cmd_pool = 0 as VkCommandPool;
         vulkan_check!(vkCreateCommandPool(
             device.vk_device, &vk_cmd_pool_info, 0 as *const VkAllocationCallbacks, &mut vk_cmd_pool));
