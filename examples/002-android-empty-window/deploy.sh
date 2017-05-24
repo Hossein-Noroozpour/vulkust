10861b6a88820c7f6ab530f833746ea0ff04fb12
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-05-24 15:27:40 +0430

    android deploy got better

--- examples/002-android-empty-window/deploy.sh
+++ examples/002-android-empty-window/deploy.sh
@@ -2,4 +2,5 @@
 clear
 clear
 cargo build &&\
-cp target/aarch64-linux-android/debug/libapp.so ../../../Vulkust/app/src/main/jniLibs/arm64-v8a/libmain.so
+cp target/aarch64-linux-android/debug/libapp.so \
+    ../../../vulkust-and/app/src/main/jniLibs/arm64-v8a/libmain.so
