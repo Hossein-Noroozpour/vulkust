620f53cd849045ccb70c12b06cfdca037d8cbe03
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2017-05-30 02:09:17 +0430

    module change

--- vulkust/src/system/mac/mod.rs
+++ vulkust/src/system/mac/mod.rs
@@ -1,6 +1,10 @@
 #[macro_use]
 pub mod macros;
+pub mod app_delegate;
 pub mod app_kit;
-pub mod foundation;
 pub mod application;
+pub mod foundation;
+pub mod game_view_controller;
+pub mod renderer;
+pub mod shader_types;
 pub mod types;
