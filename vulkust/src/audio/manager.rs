d8b1167fe180750440d7d209b51f64c4a682eecf
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2017-07-30 08:33:15 +0430

    some audio

--- vulkust/src/audio/manager.rs
+++ vulkust/src/audio/manager.rs
@@ -7,7 +7,7 @@ use super::super::system::file::File;
 use super::{Audio};
 
 pub struct Manager {
-    pub cached: BTreeMap<u64, Weak<Audio>>,
+    pub cached: BTreeMap<u64, Weak<RefCell<Audio>>>,
     pub offsets: Vec<u64>,
 }
 
@@ -25,5 +25,42 @@ impl Manager {
         for i in 0..count as usize {
             self.offsets[i] = file.read_type();
         }
+    }
+
+    pub fn get<CoreApp>(
+        &mut self,
+        id: u64,
+        file: &mut File,
+        os_app: *mut OsApplication<CoreApp>,
+    ) -> Arc<RefCell<Audio>>
+    where
+        CoreApp: ApplicationTrait,
+    {
+        match self.cached.get(&id) {
+            Some(res) => {
+                match res.upgrade() {
+                    Some(res) => {
+                        return res;
+                    }
+                    None => {}
+                }
+            }
+            None => {}
+        }
+        let offset = self.offsets[id as usize];
+        match file.seek(SeekFrom::Start(offset)) {
+            Ok(o) => {
+                if o < offset {
+                    logf!("Seeked offset does not match!");
+                }
+            }
+            _ => {
+                logf!("Can not seek to the requested offset.");
+            }
+        }
+        let aud = Perspective::new(file, os_app);
+        let camera: Arc<RefCell<Camera<f32>>> = Arc::new(RefCell::new(camera));
+        self.cached.insert(id, Arc::downgrade(&camera));
+        return camera;
     }
 }
--- vulkust/src/audio/mod.rs
+++ vulkust/src/audio/mod.rs
@@ -1,3 +1,11 @@
 pub mod manager;
 
 pub trait Audio {}
+
+pub struct Music {}
+
+impl Audio for Music {}
+
+pub struct Voice {}
+
+impl Audio for Voice {}
