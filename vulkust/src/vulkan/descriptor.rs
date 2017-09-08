724711a9ead65237e34d94e4c8d70b340c2416e4
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-09-08 20:21:38 +0430

    descriptor set limmit fixed

--- vulkust/src/vulkan/descriptor.rs
+++ vulkust/src/vulkan/descriptor.rs
@@ -23,13 +23,13 @@ impl Pool {
     pub fn new(logical_device: Arc<LogicalDevice>) -> Self {
         let mut type_counts = [vk::VkDescriptorPoolSize::default(); 1];
         type_counts[0].type_ = vk::VkDescriptorType::VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-        type_counts[0].descriptorCount = 1;
+        type_counts[0].descriptorCount = 2;
         let mut descriptor_pool_info = vk::VkDescriptorPoolCreateInfo::default();
         descriptor_pool_info.sType =
             vk::VkStructureType::VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
         descriptor_pool_info.poolSizeCount = type_counts.len() as u32;
         descriptor_pool_info.pPoolSizes = type_counts.as_ptr();
-        descriptor_pool_info.maxSets = 1;
+        descriptor_pool_info.maxSets = 2;
         let mut vk_data = 0 as vk::VkDescriptorPool;
         vulkan_check!(vk::vkCreateDescriptorPool(
             logical_device.vk_data,
