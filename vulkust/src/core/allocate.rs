536cbe37655fbeb6417f8e3bae86b156a3928596
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2018-04-27 02:13:48 +0430

    allocator:: --allocator chnged alot and i think this version despite its simplicity is ok--

--- vulkust/src/core/allocate.rs
+++ vulkust/src/core/allocate.rs
@@ -1,174 +1,64 @@
-use std::collections::BTreeMap;
 use std::sync::{Arc, RwLock, Weak};
 
-use super::list::{List, Node};
-
-pub trait Object: Drop {
+pub trait Object {
     fn size(&self) -> isize;
-    fn suceeding_size(&self) -> isize;
-    fn set_suceeding_size(&mut self, isize);
     fn place(
         &mut self,
-        suceeding_range_size: isize,
         offset: isize,
-        allocator: Arc<RwLock<Allocator>>,
     );
 }
 
 pub trait Allocator {
     fn increase_size(&mut self, size: isize);
-    fn initialize(&mut self, itself: Weak<RwLock<Allocator>>);
-    fn allocate(&mut self, bag: Arc<RwLock<Object>>);
-    fn deallocate(&mut self, suceeding_range_size: isize, end_offset: isize);
-    fn coallocate(&mut self);
+    fn allocate(&mut self, obj: Arc<RwLock<Object>>);
+    fn clean(&mut self);
 }
 
-pub struct Bag {
-    front: isize,
+pub struct Memory {
+    offset: isize,
     end: isize,
     size: isize,
-    suceeding_range_size: isize,
-    allocator: Option<Arc<RwLock<Allocator>>>,
 }
 
-impl Bag {
+impl Memory {
     pub fn new(size: isize) -> Self {
-        Bag {
-            front: 0,
+        Memory {
+            offset: 0,
             end: size,
             size,
-            suceeding_range_size: 0,
-            allocator: None,
         }
     }
 }
 
-impl Drop for Bag {
-    fn drop(&mut self) {
-        vxunwrap!(vxunwrap!(self.allocator).read()).deallocate(self.suceeding_range_size, self.end);
-    }
-}
-
-impl Object for Bag {
+impl Object for Memory {
     fn size(&self) -> isize {
         self.size
     }
 
-    fn suceeding_size(&self) -> isize {
-        self.suceeding_range_size
-    }
-
-    fn set_suceeding_size(&mut self, size: isize) {
-        self.suceeding_range_size = size;
-    }
-
-    fn place(
-        &mut self,
-        suceeding_range_size: isize,
-        offset: isize,
-        allocator: Arc<RwLock<Allocator>>,
-    ) {
-        self.suceeding_range_size = suceeding_range_size;
-        self.front = offset;
+    fn place(&mut self, offset: isize) {
+        self.offset = offset;
         self.end = self.size + offset;
-        self.allocator = Some(allocator);
-    }
-}
-
-struct FakeBag {
-    suceeding_range_size: isize,
-}
-
-impl FakeBag {
-    pub fn new() -> Self {
-        FakeBag {
-            suceeding_range_size: 0,
-        }
-    }
-}
-
-impl Drop for FakeBag {
-    fn drop(&mut self) {}
-}
-
-impl Object for FakeBag {
-    fn size(&self) -> isize {
-        0
-    }
-
-    fn suceeding_size(&self) -> isize {
-        self.suceeding_range_size
-    }
-
-    fn set_suceeding_size(&mut self, size: isize) {
-        self.suceeding_range_size = size;
-    }
-
-    fn place(
-        &mut self,
-        suceeding_range_size: isize,
-        _offset: isize,
-        _allocator: Arc<RwLock<Allocator>>,
-    ) {
-        self.suceeding_range_size = suceeding_range_size;
     }
 }
 
 pub struct Container {
-    itself: Option<Weak<RwLock<Allocator>>>,
-    front: isize,
+    offset: isize,
     end: isize,
     size: isize,
+    free_offset: isize,
     free_space: isize,
-    tailing_space: isize,
-    suceeding_range_size: isize,
-    objects: List<Weak<RwLock<Object>>>,
-    space_offset_node: BTreeMap<isize, BTreeMap<isize, Arc<RwLock<Node<Weak<RwLock<Object>>>>>>>,
-    starting_object: Option<Arc<RwLock<Object>>>,
-    ending_object: Option<Arc<RwLock<Object>>>,
-    allocator: Option<Arc<RwLock<Allocator>>>,
+    objects: Vec<Weak<RwLock<Object>>>,
 }
 
 impl Container {
-    pub fn new(front: isize, size: isize) -> Self {
-        let starting_object: Arc<RwLock<Object>> = Arc::new(RwLock::new(FakeBag::new()));
-        let ending_object: Arc<RwLock<Object>> = Arc::new(RwLock::new(FakeBag::new()));
-        let mut objects = List::new();
-        objects.append(Arc::downgrade(&starting_object));
-        objects.append(Arc::downgrade(&ending_object));
-        let offsets = BTreeMap::new();
-        offsets.insert(front, vxunwrap!(objects.front()));
-        let space_offset_node = BTreeMap::new();
-        space_offset_node.insert(size, offsets);
+    pub fn new(size: isize) -> Self {
         Container {
-            itself: None,
-            front,
-            end: front + size,
+            offset: 0,
+            end: size,
             size,
+            free_offset: 0,
             free_space: size,
-            tailing_space: size,
-            suceeding_range_size: 0,
-            objects,
-            space_offset_node,
-            starting_object: Some(starting_object),
-            ending_object: Some(ending_object),
-            allocator: None,
-        }
-    }
-}
-
-impl Drop for Container {
-    fn drop(&mut self) {
-        self.space_offset_node.clear();
-        self.objects.clear();
-        self.starting_object = None;
-        self.ending_object = None;
-        self.itself = None;
-        match self.allocator {
-            Some(allocator) => {
-                vxunwrap!(allocator.read()).deallocate(self.suceeding_range_size, self.end)
-            }
-            None => (),
+            objects: Vec::new(),
         }
     }
 }
@@ -178,80 +68,46 @@ impl Object for Container {
         self.size
     }
 
-    fn suceeding_size(&self) -> isize {
-        self.suceeding_range_size
-    }
-
-    fn set_suceeding_size(&mut self, size: isize) {
-        self.suceeding_range_size = size;
-    }
-
-    fn place(
-        &mut self,
-        suceeding_range_size: isize,
-        offset: isize,
-        allocator: Arc<RwLock<Allocator>>,
-    ) {
-        self.suceeding_range_size = suceeding_range_size;
-        let translate = offset - self.front;
-        self.front = offset;
-        let mut space_offset_node = BTreeMap::new();
-        for (space, offset_node) in &self.space_offset_node {
-            let mut tree = BTreeMap::new();
-            for (offset, node) in offset_node {
-                let offset = offset + translate;
-                vxunwrap!(vxunwrap!(vxunwrap!(node.read()).value.upgrade()).write()).place(
-                    space,
-                    offset,
-                    vxunwrap!(self.itself),
-                );
-                tree.insert(offset, *node);
-            }
-            space_offset_node.insert(*space, tree);
-        }
-        self.space_offset_node = space_offset_node;
+    fn place(&mut self, offset: isize) {
+        self.offset = offset;
+        self.clean();
     }
 }
 
 impl Allocator for Container {
-    // possibility of dead lock in here! if happened I gonna manage it
     fn increase_size(&mut self, size: isize) {
-        let n = vxunwrap!(vxunwrap!(self.objects.back()).read());
-        let n = vxunwrap!(vxunwrap!(n.previous()).read());
-        let space = vxunwrap!(n.value.read()).suceeding_size();
-        let new_space = space + size;
-        vxunwrap!(n.value.write()).set_suceeding_size(new_space);
-        self.space_offset_node.insert(
-            new_space, vxunwrap!(self.space_offset_node.remove(space)));
         self.end += size;
         self.size += size;
         self.free_space += size;
-        self.tailing_space += size;
     }
 
-    fn initialize(&mut self, itself: Weak<RwLock<Allocator>>) {
-        self.itself = Some(itself);
-    }
-
-    fn allocate(&mut self, bag: Arc<RwLock<Object>>) {
-        let bag_size = vxunwrap!(bag.read()).size();
-        if bag_size > self.free_space {
-            vxlogf!("Out of space, you probably forget to increase the size.");
+    fn allocate(&mut self, obj: Arc<RwLock<Object>>) {
+        let obj_size = vxunwrap!(obj.read()).size();
+        if obj_size > self.free_space {
+            vxlogf!("Out of space, you probably forget to increase the size or cleaning the allocator.");
         }
-        match self.objects.range_mut(bag_size..).next() {
-            Some((_, ranges)) => vxunimplemented!(), // find requested space
-            None => {
-                self.coallocate();
-                self.allocate(bag);
+        vxunwrap!(obj.write()).place(self.free_offset);
+        self.objects.push(Arc::downgrade(&obj));
+        self.free_offset += obj_size;
+        self.free_space -= obj_size;
+    }
+
+    fn clean(&mut self) {
+        let mut objects = Vec::new();
+        self.free_offset = self.offset;
+        self.free_space = self.size;
+        for obj in self.objects {
+            match obj.upgrade() {
+                Some(obj) => {
+                    let size = vxunwrap!(obj.read()).size();
+                    vxunwrap!(obj.write()).place(self.free_offset);
+                    objects.push(Arc::downgrade(&obj));
+                    self.free_offset += size;
+                    self.free_space -= size;
+                },
+                None => continue,
             }
         }
-    }
-
-    fn deallocate(&mut self, suceeding_range_size: isize, end_offset: isize) {
-        vxunimplemented!();
-    }
-
-    fn coallocate(&mut self) {
-        vxunimplemented!();
+        self.objects = objects;
     }
 }
