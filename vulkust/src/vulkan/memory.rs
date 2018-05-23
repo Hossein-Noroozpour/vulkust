657e040e014bcb4a5fbc7468e8877d8bf0d1fa04
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2018-05-23 04:39:25 +0430

    fbp:: --working on memory--

--- vulkust/src/vulkan/memory.rs
+++ vulkust/src/vulkan/memory.rs
@@ -2,7 +2,7 @@ use super::device::logical::Logical as LogicalDevice;
 use super::vulkan as vk;
 
 use std::ptr::null;
-use std::sync::Arc;
+use std::sync::{Arc, RwLock, Weak};
 use std::collections::BTreeMap;
 
 pub fn allocate_with_requirements(
@@ -46,8 +46,14 @@ pub struct Memory {
 
 struct RootMemory {}
 
+pub enum Location {
+    CPU,
+    GPU,
+}
+
 pub struct Manager {
     logical_device: Arc<LogicalDevice>,
+    itself: Option<Weak<RwLock<Manager>>>,
     memories: BTreeMap<u32, RootMemory>,
 }
 
@@ -55,11 +61,41 @@ impl Manager {
     pub fn mew(logical_device: Arc<LogicalDevice>) -> Self {
         Manager {
             logical_device,
+            itself: None,
             memories: BTreeMap::new(),
         }
     }
 
-    pub fn allocate(&mut self, mem_req: &vk::VkMemoryRequirements) -> Memory {
+    pub fn get_memory_type_index(&self, mem_req: &vk::VkMemoryRequirements, l: Location) -> u32 {
+        let l = match l {
+            CPU => vk::VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+            GPU => vk::VkMemoryPropertyFlagBits::VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
+        } as u32;
+        let ref memory_prop = self.logical_device.physical_device.memory_properties;
+        let mut type_bits = mem_req.memoryTypeBits;
+        for index in 0..memory_prop.memoryTypeCount {
+            if (type_bits & 1) == 1 && 
+                ((memory_prop.memoryTypes[index as usize].propertyFlags as u32) & l) != 0 {
+                return index;
+            }
+            type_bits >>= 1;
+        }
+        vxunexpected!();
+    }
+
+    pub fn set_itself(&mut self, itself: Weak<RwLock<Manager>>) {
+        self.itself = Some(itself);
+    }
+
+    pub fn allocate(&mut self, mem_req: &vk::VkMemoryRequirements, location: Location) -> Memory {
+        let memory_type_index = self.get_memory_type_index(mem_req, location);
+        let root_memory = self.memories.get(&memory_type_index);
+        if root_memory.is_some() {
+            let root_memory = vxunwrap!(root_memory);
+            root_memory.allocate()
+        } else {
+
+        }
         vxunimplemented!();
         Memory {}
     }
