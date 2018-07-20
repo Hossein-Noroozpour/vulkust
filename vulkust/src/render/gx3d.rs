97d94733ad0d97e4d7213661fc9d32b85d1d8148
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2018-07-21 01:32:27 +0430

    gx3d:: --new read functionalities--

--- vulkust/src/render/gx3d.rs
+++ vulkust/src/render/gx3d.rs
@@ -1,12 +1,20 @@
-use super::scene::Manager as SceneManager;
 use super::super::system::file::File;
-use std::io::Read;
+use super::scene::Manager as SceneManager;
+use std::io::{BufReader, Read};
+use std::mem::{size_of, transmute};
+use std::ptr::copy;
 use std::sync::{Arc, RwLock};
 
 pub struct Gx3D {
-    file: File,
+    file: BufReader<File>,
     different_endianness: bool,
 }
+
+pub trait Readable: 'static + Sized + Default + Clone {}
+
+impl Readable for u8 {}
+impl Readable for u32 {}
+impl Readable for u64 {}
 
 impl Gx3D {
     pub fn new() -> Option<Self> {
@@ -14,7 +22,8 @@ impl Gx3D {
         if file.is_err() {
             return None;
         }
-        let mut file = vxresult!(file);
+        let file = vxresult!(file);
+        let mut file = BufReader::new(file);
         let mut endian = [0u8; 1];
         vxresult!(file.read(&mut endian));
         #[cfg(target_endian = "little")]
@@ -27,14 +36,17 @@ impl Gx3D {
         })
     }
 
-    #[cfg(debug_assertions)]
-    fn read_typed_bytes(&mut self, bytes: &mut [u8]) {
-        let n = vxresult!(self.file.read(bytes));
-        if n != bytes.len() {
-            vxunexpected!();
+    fn read_typed_bytes(&mut self, dest: *mut u8, count: usize) {
+        let mut bytes = vec![0u8; count];
+        let _n = vxresult!(self.file.read(&mut bytes));
+        #[cfg(debug_assertions)]
+        {
+            if _n != count {
+                vxunexpected!();
+            }
         }
         if self.different_endianness {
-            let mut end = bytes.len() - 1;
+            let mut end = count - 1;
             let mut start = 0;
             while start < end {
                 let tmp = bytes[start];
@@ -44,27 +56,63 @@ impl Gx3D {
                 end -= 1;
             }
         }
-    }
-
-    #[cfg(not(debug_assertions))]
-    fn read_typed_bytes(&mut self, bytes: &mut [u8]) {
-        vxresult!(self.file.read(bytes));
-        if self.different_endianness {
-            let mut end = bytes.len() - 1;
-            let mut start = 0;
-            while start < end {
-                let tmp = bytes[start];
-                bytes[start] = bytes[end];
-                bytes[end] = tmp;
-                start += 1;
-                end -= 1;
-            }
+        unsafe {
+            copy(bytes.as_ptr(), dest, count);
         }
     }
 
-    pub fn read_id() -> 
-} 
+    fn read_array_typed_bytes(&mut self, dest: *mut u8, esize: usize, count: usize) {
+        let size = esize * count;
+        let mut bytes = vec![0u8; size];
+        let _n = vxresult!(self.file.read(&mut bytes));
+        #[cfg(debug_assertions)]
+        {
+            if _n != size {
+                vxunexpected!();
+            }
+        }
+        if self.different_endianness {
+            let inc = esize - 1;
+            let mut starte = 0;
+            let mut ende = inc;
+            for _ in 0..count {
+                let mut end = ende;
+                let mut start = starte;
+                starte += esize;
+                ende += esize;
+                while start < end {
+                    let tmp = bytes[start];
+                    bytes[start] = bytes[end];
+                    bytes[end] = tmp;
+                    start += 1;
+                    end -= 1;
+                }
+            }
+        }
+        unsafe {
+            copy(bytes.as_ptr(), dest, size);
+        }
+    }
 
-pub fn import(scenemgr: &Arc<RwLock<SceneManager>>) {
+    pub fn read<T>(&mut self) -> T
+    where
+        T: Readable,
+    {
+        let mut t = T::default();
+        let buff: *mut u8 = unsafe { transmute(&mut t) };
+        self.read_typed_bytes(buff, size_of::<T>());
+        return t;
+    }
 
+    pub fn read_array<T>(&mut self) -> Vec<T>
+    where
+        T: Readable,
+    {
+        let count = self.read::<u64>() as usize;
+        let mut ts = vec![T::default(); count];
+        self.read_array_typed_bytes(unsafe { transmute(ts.as_mut_ptr()) }, size_of::<T>(), count);
+        return ts;
+    }
 }
+
+pub fn import(scenemgr: &Arc<RwLock<SceneManager>>) {}
